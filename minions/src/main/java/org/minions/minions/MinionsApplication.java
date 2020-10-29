7ce290c8163e3b14f75b01b3fce846d1c6470558
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-10-29 19:40:57 +0000

    initialized minions in minikube, a kubernetes intro for java developers

---
+++ minions/src/main/java/org/minions/minions/MinionsApplication.java
@@ -0,0 +1,13 @@
+package org.minions.minions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinionsApplication.class, args);
+    }
+
+}
---
+++ minions/src/test/java/org/minions/minions/MinionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minions.minions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
