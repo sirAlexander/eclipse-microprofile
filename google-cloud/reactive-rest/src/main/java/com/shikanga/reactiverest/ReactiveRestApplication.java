93382cbf38cb9b0a29b1ec22fc8f3d5a0d353a11
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-08-14 13:57:23 +0100

    setup of initial google cloud reactive rest project

---
+++ google-cloud/reactive-rest/src/main/java/com/shikanga/reactiverest/ReactiveRestApplication.java
@@ -0,0 +1,13 @@
+package com.shikanga.reactiverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveRestApplication.class, args);
+    }
+
+}
---
+++ google-cloud/reactive-rest/src/test/java/com/shikanga/reactiverest/ReactiveRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.reactiverest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
